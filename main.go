9927baff929b5d7a9536c16aabe8fa6f30626dad
Mohammad Kamar Uddin <kamar@Mac.lan>
2025-02-07 20:54:48 +1100

    Added main.go module and other necessary files

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type TodoItem struct {
+	Id   string `json:"id"`
+	Item string `json:"item"`
+}
+
+func main() {
+
+	var todos = make([]TodoItem, 0)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /todo", func(w http.ResponseWriter, r *http.Request) {
+		b, err := json.Marshal(todos)
+		if err != nil {
+			log.Println(err)
+		}
+		_, err = w.Write(b)
+		if err != nil {
+			log.Println(err)
+		}
+		return
+	})
+
+	mux.HandleFunc("POST /todo", func(writer http.ResponseWriter, request *http.Request) {
+		var t TodoItem
+		err := json.NewDecoder(request.Body).Decode(&t)
+
+		if err != nil {
+			log.Println(err)
+			writer.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		todos = append(todos, t)
+		writer.WriteHeader(http.StatusCreated)
+		return
+	})
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
+}
